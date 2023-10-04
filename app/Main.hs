56e888cd8c7d6f79226a400a767449f93a725526
Mistral Contrastin <madgenhetic@gmail.com>
2023-10-05 00:38:03 +0100

    add an entry point

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main (main) where
 
+import           Prelude hiding (lex)
+import           Lexer (lex)
+import           Parser (parse)
+import qualified Naive
+import qualified Data.Text.IO as T
+
 main :: IO ()
-main = pure ()
+main = do
+  contents <- T.getContents
+  let ast = parse . lex $ contents
+  print $ Naive.drive ast
