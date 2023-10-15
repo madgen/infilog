497483d2333cb3fe7f7e89ae841d8aa1c2c2e656
Mistral Contrastin <madgenhetic@gmail.com>
2023-10-15 13:54:41 +0100

    Factor out validating test output

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,6 @@
 import           Prelude hiding (lines, id, lex)
 import           Data.Foldable (traverse_)
-import           Data.IORef (newIORef, writeIORef, readIORef)
+import           Data.IORef (newIORef, writeIORef, readIORef, IORef)
 import           Data.List (isSuffixOf, intercalate)
 import           Control.Monad (when)
 import           System.Directory (listDirectory, doesFileExist)
@@ -19,37 +19,45 @@ import qualified Data.Text as T
 main :: IO ()
 main = do
   failingRef <- newIORef False
+  let validateTestOutput' = validateTestOutput failingRef
   sources <- findSources "test/cases"
   (`traverse_` sources)
     $ \source -> do
       contents <- TIO.readFile source
+      let validateTestOutput'' = validateTestOutput' source contents
+
       let ast = parse $ lex contents
       let solution = Naive.drive . compile $ ast
       let actualOutput = show solution
       let expPath = mkExpPath source "naive"
-      mExpOutput <- readExpFile expPath
-      case mExpOutput of
-        Just expOutput -> when (expOutput /= actualOutput)
-          $ do
-            writeIORef failingRef True
-            putStrLn "Mismatch between expected and actual output."
-            putStrLn $ showTestCase source contents
-            putStrLn $ "Diff (Expected at " <> expPath <> "):"
-            let diff =
-                  getGroupedDiff (lines expOutput) (lines actualOutput)
-            putStrLn $ ppDiff diff
-            replaceExp expPath actualOutput
-            putStrLn ""
-        Nothing        -> do
-          writeIORef failingRef True
-          putStrLn "There is no expected file."
-          putStrLn $ showTestCase source contents
-          putStrLn "Actual:"
-          putStrLn actualOutput
-          replaceExp expPath actualOutput
-          putStrLn ""
+      validateTestOutput'' expPath actualOutput
   failed <- readIORef failingRef
   when failed exitFailure
+
+validateTestOutput :: IORef Bool -> FilePath -> T.Text -> FilePath -> String -> IO ()
+validateTestOutput failingRef source contents expPath actualOutput = do
+  mExpOutput <- readExpFile expPath
+  case mExpOutput of
+    Just expOutput -> when (expOutput /= actualOutput)
+      $ do
+        writeIORef failingRef True
+        putStrLn "Mismatch between expected and actual output."
+        putStrLn $ showTestCase source contents
+        putStrLn ""
+        putStrLn $ "Diff (Expected at " <> expPath <> "):"
+        let diff =
+              getGroupedDiff (lines expOutput) (lines actualOutput)
+        putStrLn $ ppDiff diff
+        replaceExp expPath actualOutput
+        putStrLn ""
+    Nothing        -> do
+      writeIORef failingRef True
+      putStrLn "There is no expected file."
+      putStrLn $ showTestCase source contents
+      putStrLn "Actual:"
+      putStrLn actualOutput
+      replaceExp expPath actualOutput
+      putStrLn ""
 
 mkExpPath :: FilePath -> String -> FilePath
 mkExpPath source suffix = source <> "." <> suffix <> ".exp"
